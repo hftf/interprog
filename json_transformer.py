731d063f4487f9abf38e175ee0c503b80605a06a
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-09-06 22:26:59 -0400

    Json transformer

--- json_transformer.py
+++ json_transformer.py
@@ -1,21 +1,54 @@
 import nexus
 import json
 
-class JSONTransformer:
-    def transform_program(program):
-    	pass
+def write_program(program):
+    return {
+        'type': 'program',
+        'functions': [write_function(i) for i in program.functions],
+        'global_scope': write_scope(program.global_scope),
+        'main_func': write_function(program.main_func)
+    }
 
-    def transform_function(function):
-    	pass
+def write_function(function):
+    return {
+        'type': 'function',
+        'block': function.block
+    }
 
-    def transform_block(block):
-    	pass
+def write_block(block):
+    return {
+        'type': 'block',
+        'scope': write_scope(block.scope),
+        'instructions': [write_instruction(i) for i in block.instructions]
+    }
 
-    def transform_scope(scope):
-    	pass
+def write_scope(scope):
+    return {
+        'type': 'scope',
+        'variables': [write_variable(i) for i in scope.variables]
+    }
 
-    def transform_variable(variable):
-    	pass
+def write_variable(variable):
+    return {
+        'type': 'variable',
+        'name': variable.name
+    }
 
-    def transform_instruction(instruction):
-    	pass
+def write_assignment(assignment):
+    return {
+        'type': 'assignment',
+        'lhs': assignment.lhs,
+        'rhs': assignment.rhs
+    }
+
+def write_expression(expression):
+    return {
+        'type': 'expression',
+        'expr': expression.expr
+    }
+
+def write_return(return):
+    return {
+        'type': 'return',
+        'var': return.var
+    }
