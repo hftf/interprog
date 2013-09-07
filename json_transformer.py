3e71143a657aae3f8a9680562cd188c843a770b1
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-09-06 23:25:28 -0400

    Finished json reader and fixed the writer

--- json_transformer.py
+++ json_transformer.py
@@ -1,13 +1,58 @@
-import nexus
+from nexus import *
 import json
 
-def write_program(program):
+def read_program(json_):
+    program = json.loads(json_)
+    return Program(
+        [read_function(i) for i in program['functions']],
+        read_scope(program['global_scope']),
+        read_function(program['main_func'])
+    )
+
+def read_function(function):
+    return Function(
+        read_block(function['block']),
+        [read_param(i) for i in function['params']]
+    )
+
+def read_block(block):
+    return Block(
+        read_scope(block['scope']),
+        [read_instruction(i) for i in block['instructions']]
+    )
+
+def read_scope(scope):
+    return Scope(
+        [read_variable(i) for i in scope['variables']]
+    )
+
+def read_variable(variable):
+    return Variable(variable['name'])
+
+def read_instruction(instruction):
     return {
+        'assignment': read_assignment,
+        'expression': read_expression,
+        'return': read_return
+    }[instruction['type']](instruction)
+
+def read_assignment(assignment):
+    return Assignment(assignment['lhs'], assignment['rhs'])
+
+def read_expression(expression):
+    return Expression(expression['expression'])
+
+def read_return(return_):
+    return Return(return_['var'])
+
+
+def write_program(program):
+    return json.dumps({
         'type': 'program',
         'functions': [write_function(i) for i in program.functions],
         'global_scope': write_scope(program.global_scope),
         'main_func': write_function(program.main_func)
-    }
+    })
 
 def write_function(function):
     return {
@@ -45,11 +90,11 @@ def write_assignment(assignment):
 def write_expression(expression):
     return {
         'type': 'expression',
-        'expr': expression.expr
+        'expression': expression.expr
     }
 
-def write_return(return):
+def write_return(return_):
     return {
         'type': 'return',
-        'var': return.var
+        'var': return_.var
     }
