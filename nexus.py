cd8a31af0bf4e6b0f8660cf052c28dc6c68b4f3b
Scott Lawrence <bytbox@gmail.com>
2013-09-06 22:05:08 -0400

    More nexus

--- nexus.py
+++ nexus.py
@@ -2,32 +2,44 @@ class Program:
     def __init__(self, functions=None, global_scope=None, main_func=None):
         if functions is None:
             self.functions = []
+        else:
+            self.functions = functions
         if global_scope is None:
             self.global_scope = Scope()
+        else:
+            self.global_scope = global_scope
         if main_func is None:
             self.main_func = Function()
+        else:
+            self.main_func = main_func
 
 class Function:
     def __init__(self, block=None):
         if block is None:
-            self.block = Block()
+            block = Block()
+        self.block = block
 
 class Block:
-    def __init__(self, scope=None, instructions=None):
-        if scope is None:
-            self.scope = Scope()
-        if instructions is None:
-            self.instructions = []
+    def __init__(self, scope, instructions):
+        self.scope = scope
+        self.instructions = instructions
 
 class Scope:
     def __init__(self, variables=None):
         if variables is None:
             variables = {}
+        self.variables = variables
 
 class Variable:
     def __init__(self, name):
         self.name = name
 
-class Instruction:
-    def __init__(self):
-        pass
+class Assignment:
+    def __init__(self, lhs, rhs=None):
+        self.lhs = lhs
+
+class Statement:
+    pass
+
+class Return:
+    pass
